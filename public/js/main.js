13d376659cfa0e204ea355ad05fb5d93410d2402
devy-wtf <dasolisg.2005fwd@gmail.com>
2025-08-13 08:59:11 -0600

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,26 @@
-console.log("hola esto es una prueba para github")
+import { crearProducto, actualizarProducto, eliminarProducto, cargarProductos } from '/services/peticionesProductos.js';
+
+
+
+const form = document.getElementById('productosForm');
+const lista = document.getElementById('listaProductos')
+
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const id = document.getElementById('productoID').value;
+  const nombre = document.getElementById('nombreProducto').value;
+  const precio = parseFloat(document.getElementById('precioProducto').value);
+  const stock = parseInt(document.getElementById('stock').value);
+
+  const producto = { nombre, precio, stock };
+
+  if (id) {
+    await actualizarProducto(id, producto);
+  } else {
+    await crearProducto(producto);
+  }
+
+  form.reset();
+  cargarProductos();
+});
---
+++ public/services/peticionesProductos.js
@@ -0,0 +1,50 @@
+const API_URL = "http://localhost:3000/productos";
+
+export async function cargarProductos() {
+
+    const res = fetch(API_URL)
+    const productos = await res.json
+
+    const lista = document.getElementsById('listaProductos')
+    lista.innerHTML = ''
+
+   productos.forEach(producto => {
+    const li = document.createElement('li');
+    li.textContent = `${producto.nombre} - $${producto.precio} - Stock: ${producto.stock}`;
+
+    const btnEditar = document.createElement('button');
+    btnEditar.textContent = 'Editar';
+    btnEditar.onclick = () => window.cargarFormulario(producto);
+
+    const btnEliminar = document.createElement('button');
+    btnEliminar.textContent = 'Eliminar';
+    btnEliminar.onclick = () => window.eliminarProducto(producto.id);
+
+    li.appendChild(btnEditar);
+    li.appendChild(btnEliminar);
+    lista.appendChild(li);
+  });
+}
+
+export async function crearProducto(producto) {
+  await fetch(API_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(producto)
+  });
+}
+
+export async function actualizarProducto(id, producto) {
+  await fetch(`${API_URL}/${id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(producto)
+  });
+}
+
+export async function eliminarProducto(id) {
+  await fetch(`${API_URL}/${id}`, {
+    method: 'DELETE'
+  });
+}
+
